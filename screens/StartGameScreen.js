3a74dbcd83c68a7b9c77ed27795e55fcb53ef634
rony646 <rony646@users.noreply.github.com>
2020-10-10 10:53:14 -0300

    more styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card'
+import Colors from '../constants/colors'
+
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a new game!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}}/>
-                    <Button title="Confirm" onPress={() => {}}/>
+                    <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
@@ -26,7 +29,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
-        marginVertical: 10,
+        marginVertical: 15,
     },
     inputContainer: {
         width: 300,
@@ -38,6 +41,9 @@ const styles = StyleSheet.create({
         width: "100%",
         justifyContent: "space-around",
         paddingHorizontal: 15
+    },
+    button: {
+        width: 90
     }
 })
 
