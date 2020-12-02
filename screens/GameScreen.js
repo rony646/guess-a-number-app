46d01a9d99c8ca25f51361ca9c741e5a63869faf
rony646 <rony646@users.noreply.github.com>
2020-12-02 15:43:14 -0300

    reponsive fixes

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {View, Text, Alert, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, Alert, StyleSheet, ScrollView, Dimensions} from 'react-native';
 import { Ionicons } from '@expo/vector-icons/'
 
 import NumberContainer from '../components/NumberContainer';
@@ -89,7 +89,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: "row",
         justifyContent: "space-around",
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: "90%"
     },
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     },
     listContainer: {
         flex: 1,
-        width: "80%"
+        width: Dimensions.get('window').width > 500 ? "60%": "80%"
     },
     list: {
        flexGrow: 1,
